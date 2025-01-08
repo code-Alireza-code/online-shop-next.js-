2522a5ab1e241c9cef00f4e56a5bcd81dea50eeb
Alireza Mohammmadi <alireza-dev@Alirezas-Mac-mini.local>
2025-01-08 22:43:16 +0330

    add react-hot-toast

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import SendOTPForm from "./SendOTPForm";
 import http from "@/services/httpService";
+import toast from "react-hot-toast";
 
 function AuthPage() {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -15,9 +16,9 @@ function AuthPage() {
     e.preventDefault();
     try {
       const { data } = await http.post("/user/get-otp", { phoneNumber });
-      console.log(data.data);
+      toast.success(data.data.message);
     } catch (error) {
-      console.log(
+      toast.error(
         error.response.data.message | "خطا درهنگام ارسال کد اعتبارسنجی"
       );
     }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import vazirFont from "@/constants/localFonts";
 import "./globals.css";
 import Header from "@/components/Header";
+import { Toaster } from "react-hot-toast";
 
 export const metadata = {
   title: "next shop",
@@ -11,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
       <body className={`${vazirFont.variable} font-sans`}>
+        <Toaster />
         <Header />
         <div className="container xl:max-w-screen-xl">{children}</div>
       </body>
