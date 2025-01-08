5e3777ef756d89147993aa4dda1390223d44ce6b
Alireza Mohammmadi <alireza-dev@Alirezas-Mac-mini.local>
2025-01-08 23:28:28 +0330

    remake sendotp query  with react-query

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -1,8 +1,10 @@
+"use client";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-function Providers(children) {
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
+function Providers({ children }) {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -4,9 +4,14 @@ import { useState } from "react";
 import SendOTPForm from "./SendOTPForm";
 import http from "@/services/httpService";
 import toast from "react-hot-toast";
+import { useMutation } from "@tanstack/react-query";
+import { getOtpApi } from "@/services/authService";
 
 function AuthPage() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const { data, isPending, error, mutateAsync } = useMutation({
+    mutationFn: getOtpApi,
+  });
 
   const handlePhoneNumber = (e) => {
     setPhoneNumber(e.target.value);
@@ -15,8 +20,8 @@ function AuthPage() {
   const handleSendOTP = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await http.post("/user/get-otp", { phoneNumber });
-      toast.success(data.data.message);
+      const data = await mutateAsync({ phoneNumber });
+      toast.success(data);
     } catch (error) {
       toast.error(
         error.response.data.message | "خطا درهنگام ارسال کد اعتبارسنجی"
