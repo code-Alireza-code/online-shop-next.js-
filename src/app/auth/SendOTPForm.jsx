7e3c9040c053925fdfa438e7c2cbcba2375f2675
Alireza Mohammmadi <alireza-dev@Alirezas-Mac-mini.local>
2025-01-08 22:36:31 +0330

    add sendOTP logic

--- src/app/auth/SendOTPForm.jsx
+++ src/app/auth/SendOTPForm.jsx
@@ -1,9 +1,9 @@
 import TextField from "@/common/TextField";
 
-function SendOTPForm({ phoneNumber, onChange }) {
+function SendOTPForm({ phoneNumber, onChange, onSubmit }) {
   return (
     <div>
-      <form className="space-y-10">
+      <form className="space-y-10" onSubmit={onSubmit}>
         <TextField
           label="شماره موبایل"
           name="phoneNumber"
--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -2,16 +2,35 @@
 
 import { useState } from "react";
 import SendOTPForm from "./SendOTPForm";
+import http from "@/services/httpService";
 
 function AuthPage() {
   const [phoneNumber, setPhoneNumber] = useState("");
+
   const handlePhoneNumber = (e) => {
     setPhoneNumber(e.target.value);
   };
+
+  const handleSendOTP = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await http.post("/user/get-otp", { phoneNumber });
+      console.log(data.data);
+    } catch (error) {
+      console.log(
+        error.response.data.message | "خطا درهنگام ارسال کد اعتبارسنجی"
+      );
+    }
+  };
+
   return (
     <div className="flex justify-center">
       <div className="w-full sm:max-w-sm">
-        <SendOTPForm phoneNumber={phoneNumber} onChange={handlePhoneNumber} />
+        <SendOTPForm
+          phoneNumber={phoneNumber}
+          onChange={handlePhoneNumber}
+          onSubmit={handleSendOTP}
+        />
       </div>
     </div>
   );
