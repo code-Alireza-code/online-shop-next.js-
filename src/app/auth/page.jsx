7ef6427c19d5a04efdd49b71a4ada1dcc926f837
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-01-09 11:59:41 +0330

    add checkotp component and logic for OTP verification

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -5,12 +5,20 @@ import SendOTPForm from "./SendOTPForm";
 import http from "@/services/httpService";
 import toast from "react-hot-toast";
 import { useMutation } from "@tanstack/react-query";
-import { getOtpApi } from "@/services/authService";
+import { checkotpApi, getOtpApi } from "@/services/authService";
+import CheckOTPForm from "./CheckOTPForm";
 
 function AuthPage() {
   const [phoneNumber, setPhoneNumber] = useState("");
-  const { data, isPending, error, mutateAsync } = useMutation({
+  const [otp, setOtp] = useState("");
+  const [step, setStep] = useState(2);
+
+  const { isPending: isSending, mutateAsync: mutateSendOtp } = useMutation({
     mutationFn: getOtpApi,
+  });
+
+  const { isPending: isChecking, mutateAsync: mutatecheckOtp } = useMutation({
+    mutationFn: checkotpApi,
   });
 
   const handlePhoneNumber = (e) => {
@@ -25,8 +33,9 @@ function AuthPage() {
       return;
     }
     try {
-      const data = await mutateAsync({ phoneNumber });
+      const data = await mutateSendOtp({ phoneNumber });
       toast.success(data.message);
+      setStep(2);
     } catch (error) {
       toast.error(
         error?.response?.data?.message | "خطا درهنگام ارسال کد اعتبارسنجی"
@@ -34,16 +43,39 @@ function AuthPage() {
     }
   };
 
+  const handleCheckOTP = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await mutatecheckOtp({ phoneNumber, otp });
+      toast.success(data.message);
+    } catch (error) {
+      toast.error(error?.response?.data?.message | "کد تایید اشتباه است");
+    }
+  };
+
+  const renderSteps = () => {
+    switch (step) {
+      case 1:
+        return (
+          <SendOTPForm
+            phoneNumber={phoneNumber}
+            onChange={handlePhoneNumber}
+            onSubmit={handleSendOTP}
+            isLoading={isSending}
+          />
+        );
+      case 2:
+        return (
+          <CheckOTPForm otp={otp} setOtp={setOtp} onSubmit={handleCheckOTP} />
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className="flex justify-center">
-      <div className="w-full sm:max-w-sm">
-        <SendOTPForm
-          phoneNumber={phoneNumber}
-          onChange={handlePhoneNumber}
-          onSubmit={handleSendOTP}
-          isLoading={isPending}
-        />
-      </div>
+      <div className="w-full sm:max-w-sm">{renderSteps()}</div>
     </div>
   );
 }
--- src/services/authService.js
+++ src/services/authService.js
@@ -4,3 +4,8 @@ export async function getOtpApi(data) {
   await new Promise((res) => setTimeout(res, 2000));
   return http.post("/user/get-otp", data).then(({ data }) => data.data);
 }
+
+export async function checkotpApi(data) {
+  await new Promise((res) => setTimeout(res, 1500));
+  return http.post("/user/check-otp")
+}
