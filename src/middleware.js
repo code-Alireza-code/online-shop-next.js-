77f10a4d9f6d2ce49f8d83591be3dab218431f5c
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-01-12 11:54:31 +0330

    add protection for admin panel and refactor code

--- src/middleware.js
+++ src/middleware.js
@@ -1,38 +1,29 @@
 import { NextResponse } from "next/server";
+import middlewareAuth from "./utils/middlewareAuth";
 
-export function middleware(req) {
+export async function middleware(req) {
   const url = req.url;
   const pathname = req.nextUrl.pathname;
 
   if (pathname.startsWith("/profile")) {
-    let strCookie = "";
-    req.cookies.getAll().forEach((item) => {
-      strCookie += `${item?.name}=${item?.value}; `;
-    });
-
-    return fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/profile`, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        Cookie: strCookie,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        const { user } = data || {};
-        if (!user) {
-          return NextResponse.redirect(new URL("/auth", url));
-        }
-
-        return NextResponse.next();
-      });
+    const user = await middlewareAuth(req);
+    if (!user) {
+      return NextResponse.redirect(new URL("/auth", url));
+    }
   }
 
   if (pathname.startsWith("/admin")) {
-    console.log("admin req");
+    const user = await middlewareAuth(req);
+
+    if (!user) {
+      return NextResponse.redirect(new URL("/auth", url));
+    }
+    if (user && user.role !== "ADMIN") {
+      return NextResponse.redirect(new URL("/profile", url));
+    }
   }
 }
 
 export const config = {
-  matcher: ["/admin", "/profile"],
+  matcher: ["/admin/:path*", "/profile/:path*"],
 };
