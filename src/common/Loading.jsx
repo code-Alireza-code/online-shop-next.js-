0c6c7fb84930cdfebbe9263138d0be6df0f43bd2
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-01-14 10:46:12 +0330

    fix margin in Loading component

--- src/common/Loading.jsx
+++ src/common/Loading.jsx
@@ -1,9 +1,9 @@
 import { BeatLoader } from "react-spinners";
 
-function Loading({ size = 15, margin = 4, color = "#000" }) {
+function Loading({ size = 15, margin = "m-4", color = "#000" }) {
   return (
-    <div className={`flex justify-center m-4`}>
-      <BeatLoader size={size} margin={margin} color={color} />
+    <div className={`flex justify-center ${margin}`}>
+      <BeatLoader size={size} color={color} />
     </div>
   );
 }
