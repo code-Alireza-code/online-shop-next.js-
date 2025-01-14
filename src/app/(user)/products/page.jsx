9fc9daff243fa145657a1a7ad807f73ceea90c61
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-01-14 23:38:07 +0330

    add product page layout and styles

--- src/app/(user)/products/page.jsx
+++ src/app/(user)/products/page.jsx
@@ -1,5 +1,35 @@
-function ProductPage() {
-  return <div>products page</div>;
+import { getAllCategories } from "@/services/categoryService";
+import { getAllProducts } from "@/services/productService";
+
+async function ProductPage() {
+  const { products } = await getAllProducts();
+  const { categories } = await getAllCategories();
+
+  return (
+    <div>
+      <h1 className="text-xl font-bold mb-6">صفحه محصولات</h1>
+      <div className="grid grid-cols-4">
+        <div>
+          <p className="font-bold mb-4">دسته بندی ها</p>
+          <ul className="col-span-1 space-y-4">
+            {categories.map((category) => (
+              <li key={category._id}>{category.title}</li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-span-3 grid grid-cols-3 gap-4">
+          {products.map((product) => (
+            <div
+              key={product._id}
+              className="col-span-1 border rounded-xl shadow-md p-4 flex items-center"
+            >
+              <h2 className="font-bold">{product.title}</h2>
+            </div>
+          ))}
+        </div> 
+      </div>
+    </div>
+  );
 }
 
 export default ProductPage;
