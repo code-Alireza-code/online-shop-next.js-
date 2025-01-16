74303ec45638b72f278b34e891cd26954a7dac75
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-01-16 12:18:32 +0330

    remove default option from toLocalDate function

--- src/utils/toLocalDate.js
+++ src/utils/toLocalDate.js
@@ -1,8 +1,3 @@
-export function toLocalDateString(date, options = null) {
-  const defaultOptions = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  return new Date(date).toLocaleDateString("fa-IR", options || defaultOptions);
+export function toLocalDateString(date, options = {}) {
+  return new Date(date).toLocaleDateString("fa-IR", options);
 }
