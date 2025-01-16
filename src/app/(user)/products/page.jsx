74a452fdcfaa0525a53078978058e14ad244c7ae
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-01-16 12:19:18 +0330

    add more detail to products page

--- src/app/(user)/products/page.jsx
+++ src/app/(user)/products/page.jsx
@@ -2,6 +2,8 @@ import { getAllCategories } from "@/services/categoryService";
 import { getAllProducts } from "@/services/productService";
 import CategorySidebar from "./CategorySidebar";
 import queryString from "query-string";
+import { toLocalDateString } from "@/utils/toLocalDate";
+import Link from "next/link";
 
 async function ProductPage({ searchParams }) {
   const { products } = await getAllProducts(
@@ -14,15 +16,29 @@ async function ProductPage({ searchParams }) {
       <h1 className="text-xl font-bold mb-6">صفحه محصولات</h1>
       <div className="grid grid-cols-4">
         <CategorySidebar categories={categories} />
-        <div className="col-span-3 grid grid-cols-3 gap-4">
-          {products.map((product) => (
-            <div
-              key={product._id}
-              className="col-span-1 border rounded-xl shadow-md p-4 flex items-center"
-            >
-              <h2 className="font-bold">{product.title}</h2>
-            </div>
-          ))}
+        <div className="col-span-3">
+          <div className="grid grid-cols-3 gap-4">
+            {products.map((product) => (
+              <div
+                key={product._id}
+                className="col-span-1 border rounded-xl shadow-md p-4"
+              >
+                <h2 className="font-bold text-xl mb-4">{product.title}</h2>
+                <div className="mb-4">
+                  <span>تاریخ ساخت : </span>
+                  <span className="font-bold">
+                    {toLocalDateString(product.createdAt)}
+                  </span>
+                </div>
+                <Link
+                  href={`/products/${product.slug}`}
+                  className="font-bold text-primary-900"
+                >
+                  مشاهده محصول
+                </Link>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
