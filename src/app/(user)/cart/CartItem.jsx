746efeace5217680b8e47ff2951f2fce325d5a84
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-01-18 22:03:24 +0330

    improve CartItem component

--- src/app/(user)/cart/CartItem.jsx
+++ src/app/(user)/cart/CartItem.jsx
@@ -1,11 +1,31 @@
 import { HiMinus, HiOutlineTrash, HiPlus } from "react-icons/hi";
 
 function CartItem({ product }) {
+  console.log(product.discount);
+
   return (
     <div className="border rounded-xl p-4 flex justify-between">
       <span className="flex-1 font-bold">{product.title}</span>
+      <div className="ml-3 flex items-center gap-x-3 border-l-2 pl-4">
+        <span>قیمت :</span>
+        {product.discount > 0 && (
+          <>
+            <span className="font-bold">{product.offPrice}</span>
+            <div className=" text-white rounded-2xl px-2 py-1 flex items-center justify-center bg-red-500">
+              %{product.discount}
+            </div>
+          </>
+        )}
+        <span
+          className={` ${
+            product.discount > 0 ? "line-through text-gray-600" : "font-bold"
+          }`}
+        >
+          {product.price}
+        </span>
+      </div>
       <div className="flex items-center justify-between gap-x-8">
-        <span>تعداد : {product.quantity}</span>
+        <span className=" border-l-2 pl-4">تعداد : {product.quantity}</span>
         <div className="flex gap-x-2">
           <button className="bg-primary-900 text-white rounded p-1">
             <HiPlus className="w-4 h-4" />
