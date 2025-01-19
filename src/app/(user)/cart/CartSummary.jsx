2ae6bf413f4baef75fd6622af7d461b3fc01738e
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-01-19 11:48:43 +0330

    add confirm payment logic

--- src/app/(user)/cart/CartSummary.jsx
+++ src/app/(user)/cart/CartSummary.jsx
@@ -1,8 +1,27 @@
+import Loading from "@/common/Loading";
+import { createPaymentApi } from "@/services/paymentService";
 import discountCalculator from "@/utils/dicountCalculator";
 import { toPersianNumbersWithComma } from "@/utils/toPersianNumbers";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 function CartSummary({ payDetail }) {
   const { totalOffAmount, totalPrice, totalGrossPrice } = payDetail;
+  const queryClient = useQueryClient();
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: createPaymentApi,
+  });
+
+  const handleCompleteOrder = async () => {
+    try {
+      const { message } = await mutateAsync();
+      toast.success(message);
+      queryClient.invalidateQueries({ queryKey: ["get-user"] });
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  };
 
   return (
     <div className="border px-4 py-8 rounded-es-lg flex flex-col gap-y-6 text-sm">
@@ -21,9 +40,16 @@ function CartSummary({ payDetail }) {
         </span>
         <span>{toPersianNumbersWithComma(totalOffAmount)}تومان</span>
       </div>
-      <button className="btn btn--primary shadow-sm rounded-md -mb-2 text-md">
-        تایید و تکمیل سفارش
-      </button>
+      {isPending ? (
+        <Loading />
+      ) : (
+        <button
+          onClick={handleCompleteOrder}
+          className="btn btn--primary shadow-sm rounded-md -mb-2 text-md"
+        >
+          تایید و تکمیل سفارش
+        </button>
+      )}
     </div>
   );
 }
