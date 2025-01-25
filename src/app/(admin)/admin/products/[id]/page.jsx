4649875b78fcb32aed6ec7032cd7c3e14177ff0d
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-01-25 21:20:55 +0330

    add product detail page structure(not completed!)

--- src/app/(admin)/admin/products/[id]/page.jsx
+++ src/app/(admin)/admin/products/[id]/page.jsx
@@ -1,12 +1,21 @@
 "use client";
 
+import Loading from "@/common/Loading";
+import { useGetProductById } from "@/hooks/useProducts";
 import { useParams } from "next/navigation";
 
 function ProductDetailPage() {
-  const params = useParams();
-  const { id } = params;
+  const { id } = useParams();
 
-  return <div>product detail page</div>;
+  const { data, isLoading } = useGetProductById(id);
+  const { product } = data || {};
+
+  if (isLoading) return <Loading />;
+  return (
+    <div>
+      <h1 className="mb-4 font-bold text-xl">{product.title}</h1>
+    </div>
+  );
 }
 
 export default ProductDetailPage;
