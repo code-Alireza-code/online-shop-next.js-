db5d6f8a5fcda4f4ebe9add3610f891a2ed3e400
code-Alireza-code <alireza.mohammadi.m19@gmail.com>
2025-01-27 11:20:55 +0330

    modify grid cols

--- src/app/(admin)/admin/layout.jsx
+++ src/app/(admin)/admin/layout.jsx
@@ -15,11 +15,11 @@ function RootLayout({ children }) {
       <body className={`${vazirFont.variable} font-sans`}>
         <Providers>
           <Toaster />
-          <div className="grid grid-cols-4 bg-white h-screen">
+          <div className="grid grid-cols-6 bg-white h-screen">
             <div className="col-span-1 bg-gray-100 overflow-y-auto p-4">
               <AdminSideBar />
             </div>
-            <div className="col-span-3 overflow-y-auto p-4">{children}</div>
+            <div className="col-span-5 overflow-y-auto p-4">{children}</div>
           </div>
         </Providers>
       </body>
